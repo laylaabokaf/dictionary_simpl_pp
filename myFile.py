98cefb3361231058c63958182b2b561a95e26a82
laylaabokaf <abokafl@post.bgu.ac.il>
2022-03-17 18:20:53 +0200

    3 version

--- myFile.py
+++ myFile.py
@@ -1,11 +1,27 @@
 import json
+from difflib import get_close_matches
 
+def try_again(word,data):
+    decide = input("press y for yes or n for no")
+    decide = decide.lower()
+    decide = decide.strip()
+    if decide == "y":
+        for defin in data[get_close_matches(word, data.keys())[0]]:
+            print("' " + defin + " '\n")
+    elif decide == "n":
+         print("sorry we didn't find a definition for " + word + " try another one \n")
+    else:
+         print("You have entered wrong input ")
+         try_again(word,data)
 
 
 def Search_and_get_Difnition(word,data):
     if word in data:
         for defin in data[word] :
           print("' " + defin + " '\n")
+    elif len(get_close_matches(word, data.keys())) > 0:
+        print("did you mean %s instead" % get_close_matches(word, data.keys())[0])
+        try_again(word,data)
     else:
         print("sorry we didn't find a definition for " + word + " try another one \n")
 
